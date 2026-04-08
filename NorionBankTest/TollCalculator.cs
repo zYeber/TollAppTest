e58504388cd805c3d6f0fa4890880dd7c001e1d9
zYamato <jesperceriksson@gmail.com>
2026-04-08 11:25:42 +0200

    Reverse if statmement in IsTollFreeDate To make a return instead of if

--- NorionBankTest/TollCalculator.cs
+++ NorionBankTest/TollCalculator.cs
@@ -83,23 +83,17 @@ public class TollCalculator
         int month = date.Month;
         int day = date.Day;
 
-        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
+        if (date.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday) return true;
 
-        if (year == 2013)
-        {
-            if (month == 1 && day == 1 ||
-                month == 3 && (day == 28 || day == 29) ||
-                month == 4 && (day == 1 || day == 30) ||
-                month == 5 && (day == 1 || day == 8 || day == 9) ||
-                month == 6 && (day == 5 || day == 6 || day == 21) ||
-                month == 7 ||
-                month == 11 && day == 1 ||
-                month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
-            {
-                return true;
-            }
-        }
-        return false;
+        if (year != 2013) return false;
+        return month == 1 && day == 1 ||
+               month == 3 && (day == 28 || day == 29) ||
+               month == 4 && (day == 1 || day == 30) ||
+               month == 5 && (day == 1 || day == 8 || day == 9) ||
+               month == 6 && (day == 5 || day == 6 || day == 21) ||
+               month == 7 ||
+               month == 11 && day == 1 ||
+               month == 12 && (day == 24 || day == 25 || day == 26 || day == 31);
     }
 
     private enum TollFreeVehicles
